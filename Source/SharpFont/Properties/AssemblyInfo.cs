5220b7b3abfcecd82b90d49b9c177f291e607120
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-12-10 22:45:34 +0100

    Prep for NuGet package

--- Source/SharpFont/Properties/AssemblyInfo.cs
+++ Source/SharpFont/Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System;
 using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("SharpFont.HarfBuzz, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a59c5ac969a8f89975b27ed28ce6416b506c28b5e696228a3da8c3c7d3b0af84e03895c892a704399a7d98b45c065254f6f5e4099effa1959ebd60c1282c492b1629a24da36a176d033808878e48432f99ddd14592049392f7153bc9d847053b27911a9b4052f3c6d27cecb6ad8f806ac12426514227e6ffec7b18ba46d76bb6")]
+[assembly: InternalsVisibleTo("SpaceWizards.SharpFont.HarfBuzz")]
